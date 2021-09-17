605328936ed29b6f1941d5a9def0c4bc098e5ed3
503 <503@academicos.cesde.edu.co>
2021-09-17 08:52:39 -0500

    Se crea menu

--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -5,6 +5,8 @@
  */
 package empresa;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 503
@@ -15,7 +17,20 @@ public class Empresa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada=new Scanner(System.in);
+        int opcion;
+        
+        do{
+            
+            System.out.println("Bienbenido");
+            System.out.println("****************************");
+            System.out.println("Agregar emleado");
+            System.out.println("Mostrar empleado");
+            System.out.println("Mostrar empleados");
+            System.out.println("alir");
+        }while()
+        
     }
     
 }
