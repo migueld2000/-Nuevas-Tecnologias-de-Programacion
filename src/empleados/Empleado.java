f07196a67c70d92cd45b940f054ccbc50ccbf583
503 <503@academicos.cesde.edu.co>
2021-09-17 10:00:33 -0500

    Se codifica la opcion 1 y 3 del menu

--- src/empleados/Empleado.java
+++ src/empleados/Empleado.java
@@ -12,12 +12,12 @@ package empleados;
 public class Empleado {
     //atributos = datos = variables
     
-    int id;
-    String nombre;
-    String cargo;
-    int salario;
-    String telefono;
-    String direccion;
+    private int id;
+    private String nombre;
+    private String cargo;
+    private int salario;
+    private String telefono;
+    private String direccion;
     //metodos especiales = fotocopiadora = inicia la clase
     
     public Empleado() {
@@ -31,6 +31,60 @@ public class Empleado {
         this.telefono = telefono;
         this.direccion=direccion;
     }
+    
+    //Metodos de encapsulamiento
+    //get  y set
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+    
     
     //metodos = aciones = funciones
 
--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -5,6 +5,7 @@
  */
 package empresa;
 
+import empleados.Empleado;
 import java.util.Scanner;
 
 /**
@@ -20,16 +21,80 @@ public class Empresa {
         
         Scanner entrada=new Scanner(System.in);
         int opcion;
+        int contador=0;
+        
+        
+        Empleado [] empleados=new Empleado[10];
         
         do{
             
             System.out.println("Bienbenido");
             System.out.println("****************************");
-            System.out.println("Agregar emleado");
-            System.out.println("Mostrar empleado");
-            System.out.println("Mostrar empleados");
-            System.out.println("alir");
-        }while()
+            System.out.println("1. Agregar emleado");
+            System.out.println("2. Mostrar empleado");
+            System.out.println("3. Mostrar empleados");
+            System.out.println("4. Salir");
+            //System.out.println("");
+            
+            System.out.println("Digite la opcion: ");
+            opcion=entrada.nextInt();
+            
+            switch(opcion){
+            
+                case 1:
+                    //contador
+                    if ( i;empleados<=10;i++)
+                    {
+                        empleados[contador]=new Empleado();
+                        System.out.println("Digita el id del empleado: ");
+                        empleados[contador].setId(entrada.nextInt());
+                        System.out.println("Digita el nombre: ");
+                        empleados[contador].setNombre(entrada.next());
+                        System.out.println("Digite el cargo: ");
+                        empleados[contador].setCargo(entrada.next());
+                        System.out.println("Digite el telefono: ");
+                        empleados[contador].setTelefono(entrada.next());
+                        System.out.println("Digite la direccion: ");
+                        empleados[contador].setDireccion(entrada.next());
+                        
+                    }
+                    else
+                    {
+                        System.out.println("Ya no se puede ingresar mas empleados");
+                    }
+                    
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    for(int i=0; i<contador;i++)
+                    {
+                        System.out.println(empleados[i].getNombre());
+                        System.out.println(empleados[i].getCargo());
+                        System.out.println(empleados[i].getSalario());
+                        System.out.println(empleados[i].getTelefono());
+                        System.out.println(empleados[i].getDireccion());
+                        
+                    }
+                    
+                    
+                    break;
+                case 4:
+                    //msj
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+            
+            
+            
+            
+        }while(opcion!=4);
         
     }
     
